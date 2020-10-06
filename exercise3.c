9cc4987b7dff53b0e1d430691fee5693d9c74e27
benedictusalvian <bene@connect.hku.hk>
2020-10-06 20:11:40 +0800

    Add headers

--- exercise3.c
+++ exercise3.c
@@ -2,6 +2,11 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <sys/wait.h>
+#include <string.h>
+
+#include <sys/types.h>
+#include <sys/ipc.h>
+#include <sys/shm.h>
 
 int compare(const void *a, const void *b)
 {
