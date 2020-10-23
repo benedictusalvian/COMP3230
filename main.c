15199580f96c4cff27b9809becff8e456e0325c4
benedictusalvian <bene@connect.hku.hk>
2020-10-23 16:03:55 +0800

    Upload test

---
+++ main.c
@@ -0,0 +1,31 @@
+#include <unistd.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <sys/wait.h>
+#include <sys/types.h>
+#include <sys/ipc.h>
+#include <sys/shm.h>
+
+int main()
+{
+    int count = 0;
+    pid_t pid1, pid2;
+    pid1 = fork();
+    count++;
+    if (pid1 == 0)
+    {
+        pid2 = fork();
+        count++;
+        if (pid2 == 0)
+        {
+            count = count + 10;
+            exit(0);
+        }
+    }
+    else
+    {
+        count++;
+    }
+    printf(“Hello world !\n”);
+    printf(“Count is % d\n”, count);
+}
