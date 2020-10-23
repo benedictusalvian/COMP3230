226d4aabb87567209352acadd2753afdfa0ff716
benedictusalvian <bene@connect.hku.hk>
2020-10-23 16:15:34 +0800

    Fix typo

--- main.c
+++ main.c
@@ -19,7 +19,7 @@ int main()
         if (pid2 == 0)
         {
             count = count + 10;
-            print("I am pid2, count is %d\n", count);
+            printf("I am pid2, count is %d\n", count);
             exit(0);
         }
     }
